7f341da383a1abfe77a8fbe67c781f7f9a75cba8
wellerman114 <kimdonggyu114@gmail.com>
2026-09-11 17:19:01 +0900

    20260911 : question/answer

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,11 +1,27 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
-# crate_app = 애플리케이션 팩토리
+import config
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(config)
 
-    #블루프린트 등록
-    from .views import main_views
+    # ORM
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+
+    # 블루프린트
+    from .views import main_views, question_views, answer_views
     app.register_blueprint(main_views.bp)
+    app.register_blueprint(question_views.bp)
+    app.register_blueprint(answer_views.bp)
 
     return app
--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,14 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, redirect, url_for
 
-# 라우팅 함수를 체계적으로 관리해줌 = blueprint
 
-bp = Blueprint('main', __name__,url_prefix='/')
+# blueprint :  라우팅 함수를 체계적으로 관리
+bp = Blueprint('main', __name__, url_prefix='/')
+
 
 @bp.route('/')
-def hello_pybo():
-    return 'Hello pybo!'
+def index():
+    return redirect(url_for('question._list'))
 
-@bp.route('/hello')
-def hello():
-    return 'Hello page!'
+
 
